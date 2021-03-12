12b0ec982a1b12fcecbac88f9049c9a6671e8ee3
徐空文 <oabdao@gmail.com>
2021-03-12 16:51:32 +0800

    11

---
+++ docs/none-os/haha.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+bputil -d | grep "CustomerKC" | grep -v "absent"
+KC=$?
+if [ $KC -eq 1 ]
+then
+  bputil -n -k -c -a -s
+  csrutil disable
+  csrutil authenticated-root disable
+fi
+curl https://downloads.corellium.info/linux.macho > linux.macho
+kmutil configure-boot -c linux.macho -v /Volumes/Macintosh\ HD/
+echo "Kernel installed. Please reboot";
